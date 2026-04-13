1d5dabbf7204f8768c95c8299a30db945d56f593
Pavan-115 <dc0966@srmist.edu.in>
2026-04-13 15:32:58 +0530

    printing student result

---
+++ StudentResult.java
@@ -0,0 +1,16 @@
+public class StudentResult {
+    public static void main(String[] args) {
+
+        // Student details
+        String studentName = "Sam";
+        int rollNumber = 1;
+        double percentMarks = 99.99;
+        char result = 'P'; // P = Pass, F = Fail
+
+        // Display output
+        System.out.println("Student Name   : " + studentName);
+        System.out.println("Roll Number    : " + rollNumber);
+        System.out.println("Percent Marks  : " + percentMarks);
+        System.out.println("Result         : " + result);
+    }
+}
